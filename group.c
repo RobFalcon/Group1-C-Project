025eacb7d70f4b3e24bb26640c3501796eda3098
Brad <bradeibert7@gmail.com>
2019-09-21 13:28:15 -0700

    Laid out the structure in main

--- group.c
+++ group.c
@@ -25,7 +25,22 @@ void BradDeibert() {
 
 int main(void) {
 
-    
+    printf("Hello! We're group 1!\n");
+    printf("We are very sorry we couldn't come up with a better name, we hate to disappoint.\n");
+    printf("Let us make it up to you with a little about ourselves:");
+    printf("\n\n");
+
+    printf("*** BRAD DEIBERT ***\n");
+    BradDeibert();
+
+    printf("*** ROB FALCON ***\n");
+    RobFalcon();
+
+    printf("*** SAM CLEMENTS ***\n");
+    SamClements();
+
+    printf("*** DAVID FITE ***\n");
+    DavidFite();
 
 
 
