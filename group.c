01dfb3eead86cd777714af8350211f4d437be700
Brad <bradeibert7@gmail.com>
2019-09-21 13:42:27 -0700

    Finished up my personal function

--- group.c
+++ group.c
@@ -20,45 +20,37 @@ void DavidFite() {
 
 void BradDeibert() {
 
+    printf("Name: Bradley Deibert\n");
+    printf("Year: Junior\n");
+    printf("Major: Computer Science\n");
+    printf("Hobbies: Working out, playing basketball/football, wathcing the Packers & Lakers\n");
+    printf("Hometown: Kalispell, MT\n");
+    printf("Other things about me: Recently got engaged to California's sweetest girl by the bay!!!\n");
 
 }
 
 int main(void) {
 
-    printf("Hello! We're group 1!\n");
+    printf("**** HELLO! WE'RE GROUP 1! ****\n");
     printf("We are very sorry we couldn't come up with a better name, we hate to disappoint.\n");
     printf("Let us make it up to you with a little about ourselves:");
     printf("\n\n");
 
     printf("*** BRAD DEIBERT ***\n");
     BradDeibert();
+    printf("\n");
 
     printf("*** ROB FALCON ***\n");
     RobFalcon();
+    printf("\n");
 
     printf("*** SAM CLEMENTS ***\n");
     SamClements();
+    printf("\n");
 
     printf("*** DAVID FITE ***\n");
     DavidFite();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    printf("\n");
 
     return 0;
 }
