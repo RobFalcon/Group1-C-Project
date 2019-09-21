418bb1df0d250c29d9c79c58d8ab460b06169afd
Brad <bradeibert7@gmail.com>
2019-09-21 13:09:49 -0700

    Laying the groundwork

--- group.c
+++ group.c
@@ -1,5 +1,49 @@
 /**
  *		MAIN FILE FOR PROJECT
  *		INSERT CODE BELOW
- *
  */
+
+void RobFalcon() {
+
+    // Robert, code here
+}
+
+void SamClements() {
+
+    // Sam, code here
+}
+
+void DavidFite() {
+
+    // David, code here
+}
+
+void BradDeibert() {
+
+
+}
+
+int main(void) {
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
